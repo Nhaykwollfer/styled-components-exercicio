b928ddac2f3c796f6481d48d5d6849b9d044a851
Nhaykwollfer <108698094+Nhaykwollfer@users.noreply.github.com>
2022-10-20 19:11:26 -0300

    Exercício Concluído

--- src/Componentes/CardVideo.js
+++ src/Componentes/CardVideo.js
@@ -1,12 +1,14 @@
+import { MeuCardVid } from "./Styled";
+
 function CardVideo(props) {
   function reproduzVideo(props) {
     alert("O vídeo está sendo reproduzido");
   }
   return (
-    <div className="box-pagina-principal" onClick={reproduzVideo}>
+    <MeuCardVid className="box-pagina-principal" onClick={reproduzVideo}>
       <img src={props.image1} alt={props.textoAlternativo} />
       <h4>{props.titulo}</h4>
-    </div>
+    </MeuCardVid>
   );
 }
 
---
+++ src/Componentes/Styled.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const MeuReader = styled.header`
+    background-color: red;
+    color: white;
+`
+export const MinhaMain = styled.main`
+    background-color: orange;
+`
+export const MinhaNav = styled.nav`
+    display: grid;
+    background-color: black;
+    color: white;
+`
+export const MeuCard = styled.section`
+    display; grid;
+    align-items: center;
+    text-align: center;
+    color: black;
+`
+export const MeuCardVid = styled.div`
+    background-color: white;
+`
+
+export const MeuFooter = styled.footer`
+    background-color: red;
+`
